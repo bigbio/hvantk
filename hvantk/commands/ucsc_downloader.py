c021dfd0ebe6026c1e1b52a05fa32b0144c1243a
enriquea <enrique.audain@gmail.com>
2025-05-02 14:13:15 +0200

    Add UCSC downloader CLI for dataset retrieval

---
+++ hvantk/commands/ucsc_downloader.py
@@ -0,0 +1,108 @@
+import click
+
+from hvantk.datasets.ucsc_cell_datasets import UCSCDataSetCollection
+from hvantk.settings import CONTEXT_SETTINGS
+from hvantk.utils.file_utils import download_file
+from hvantk.utils.constants import (
+    UCSC_CELL_BROWSER_BASE_URL,
+    EXPRESSION_MATRIX_FILE_NAME,
+    METADATA_FILE_NAME,
+    UCSC_JSON_FILE_PATH,
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    """A package for gene and variant annotation."""
+    pass
+
+
+def _print_dataset_names():
+    """
+    Prints the names of all available UCSC datasets in a formatted list.
+
+    :raises FileNotFoundError: If the JSON file containing UCSC dataset
+                               information is not found at the specified
+                               path.
+    :raises JSONDecodeError: If the JSON file contains invalid JSON format.
+    """
+    try:
+        collection = UCSCDataSetCollection.from_json(UCSC_JSON_FILE_PATH)
+        dataset_names = collection.list_dataset_names()
+        if not dataset_names:
+            click.echo("No datasets available.")
+        else:
+            click.echo("Available datasets:")
+            for name in dataset_names:
+                click.echo(f"- {name}")
+    except ValueError as e:
+        click.echo(f"Error: {e}")
+
+
+@click.command("ucsc-downloader", short_help="Download UCSC Cell Browser data")
+@click.option(
+    "--dataset",
+    default="adultPancreas",
+    type=str,
+    required=True,
+    help="Dataset to download from UCSC Cell Browser.",
+)
+@click.option(
+    "--output-dir",
+    default="data/ucsc",
+    type=str,
+    required=False,
+    help="Output directory for downloaded files.",
+)
+@click.option(
+    "--base_url",
+    default=UCSC_CELL_BROWSER_BASE_URL,
+    type=str,
+    required=False,
+    help="URL to download the data from.",
+)
+@click.option(
+    "--list_datasets",
+    is_flag=True,
+    required=False,
+    help="List all available datasets in the UCSC collection.",
+)
+@click.pass_context
+def ucsc_downloader(ctx, dataset, output_dir, base_url, list_datasets):
+    """
+    Download expression matrix and metadata from the UCSC Cell Browser for a specified dataset (e.g., adultPancreas).
+
+    Usage examples:
+        # Download a specific dataset
+        hvantk ucsc-downloader --dataset <dataset_name> --output-dir <output_directory>
+
+        # Download a specific dataset with a custom base URL
+        hvantk ucsc-downloader --dataset <dataset_name> --output-dir <output_directory> --base_url <custom_base_url>
+
+        # List all available datasets
+        hvantk ucsc-downloader --list_datasets
+    """
+
+    if list_datasets:
+        _print_dataset_names()
+        return
+
+    # Construct the URLs for the expression matrix and metadata files
+    url_expression_matrix = (
+        f"{base_url}/{dataset}/{EXPRESSION_MATRIX_FILE_NAME}"
+    )
+    url_metadata = f"{base_url}/{dataset}/{METADATA_FILE_NAME}"
+
+    # make output dir as: output_dir/dataset
+    output_dir = f"{output_dir}/{dataset}"
+
+    # Download the expression matrix file
+    download_file(url_expression_matrix, output_dir, EXPRESSION_MATRIX_FILE_NAME)
+    # Download the metadata file
+    download_file(url_metadata, output_dir, METADATA_FILE_NAME)
+
+    click.echo(f"Data downloaded to {output_dir}")
+
+
+if __name__ == "__main__":
+    cli()
