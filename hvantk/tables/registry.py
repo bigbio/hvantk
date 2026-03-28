e57a3412511cd0bb3f65a880abcc130cb5deb032
enriquea <enrique.audain@gmail.com>
2026-03-28 12:25:56 +0100

    fix: add eQTL and pQTL table builders to registry for improved data processing

--- hvantk/tables/registry.py
+++ hvantk/tables/registry.py
@@ -270,6 +270,12 @@ TABLE_BUILDERS: Dict[str, Callable[[str, str, Dict[str, Any] | None], None]] = {
     "ptm-sites": create_table_adapter(
         "hvantk.tables.table_builders", "create_ptm_sites_tb"
     ),
+    "eqtl": create_table_adapter(
+        "hvantk.tables.table_builders", "create_eqtl_tb"
+    ),
+    "pqtl": create_table_adapter(
+        "hvantk.tables.table_builders", "create_pqtl_tb"
+    ),
 }
 
 
